6975056bb7e0d67f8e1b11b5fd276da3307fe762
kazmabbas <105518597+kazmabbas@users.noreply.github.com>
2023-12-10 17:36:10 +0300

    لبيك ياعلي

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -25,7 +25,7 @@ async def start(bot: Client, msg: Message):
                     InlineKeyboardButton(text="- بـدء استخـراج الجلسـة .", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/Repthon"),
+                    InlineKeyboardButton("- قنـاة السـورس .", url="https://t.me/repethone"),
                     InlineKeyboardButton("- المطـور .", user_id=OWNER_ID)
                 ]
             ]
